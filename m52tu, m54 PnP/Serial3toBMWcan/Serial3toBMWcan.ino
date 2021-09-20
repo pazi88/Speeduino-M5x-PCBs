eccbfa2559f666d010908d18477f2cd066bfc807
Pasi Kemppainen <48950874+pazi88@users.noreply.github.com>
2021-09-20 16:33:29 +0300

    Initial test

--- m52tu, m54 PnP/Serial3toBMWcan/Serial3toBMWcan.ino
+++ m52tu, m54 PnP/Serial3toBMWcan/Serial3toBMWcan.ino
@@ -10,9 +10,17 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //THE SOFTWARE.
 #include <src/STM32_CAN/STM32_CAN.h>
+#define DS2_ENABLE
+#ifdef DS2_ENABLE
+  #include "DS2.h"
+#endif
 
 #ifdef ARDUINO_BLUEPILL_F103C8
-HardwareSerial Serial3(USART3); //for some reason this isn't defined in arduino_core_stm32
+  HardwareSerial Serial3(USART3); //for some reason this isn't defined in arduino_core_stm32
+  #ifdef DS2_ENABLE
+    HardwareSerial Serial2(USART2); //for some reason this isn't defined in arduino_core_stm32
+    DS2 DS2(Serial2);
+  #endif
 #endif
 #define pin  LED_BUILTIN
 
@@ -22,6 +30,66 @@ static CAN_message_t CAN_msg_MPG_CEL;
 static CAN_message_t CAN_inMsg;
 
 STM32_CAN Can1 (_CAN1,DEF);
+
+// This struct gathers data read from speeduino
+struct statuses {
+	uint8_t secl; //secl is simply a counter that increments each second.
+	uint8_t status1; //status1 Bitfield, inj1Status(0), inj2Status(1), inj3Status(2), inj4Status(3), DFCOOn(4), boostCutFuel(5), toothLog1Ready(6), toothLog2Ready(7)
+	uint8_t engine; //Engine Status Bitfield, running(0), crank(1), ase(2), warmup(3), tpsaccaen(4), tpsacden(5), mapaccaen(6), mapaccden(7)
+	uint8_t dwell; //Dwell in ms * 10
+	uint16_t MAP; //2 bytes for MAP
+	uint8_t IAT;
+	uint8_t CLT;
+	uint8_t batCorrection; //Battery voltage correction (%)
+	uint8_t battery10; //battery voltage
+	uint8_t O2; //O2
+	uint8_t egoCorrection; //Exhaust gas correction (%)
+	uint8_t iatCorrection; //Air temperature Correction (%)
+	uint8_t wueCorrection; //Warmup enrichment (%)
+	uint16_t RPM; //rpm
+	uint8_t AEamount; //acceleration enrichment (%)
+	uint8_t corrections; //Total GammaE (%)
+	uint8_t VE; //Current VE 1 (%)
+	uint8_t afrTarget;
+	uint16_t PW1; //Pulsewidth 1 multiplied by 10 in ms. Have to convert from uS to mS.
+	uint8_t tpsDOT; //TPS DOT
+	uint8_t advance;
+	uint8_t TPS; // TPS (0% to 100%)
+	uint16_t loopsPerSecond;
+	uint16_t freeRAM;
+	uint8_t boostTarget; //boost target divided by 2 to fit in a byte
+	uint8_t boostDuty;
+	uint8_t spark; //Spark related bitfield, launchHard(0), launchSoft(1), hardLimitOn(2), softLimitOn(3), boostCutSpark(4), error(5), idleControlOn(6), sync(7)
+	uint16_t rpmDOT;
+	uint8_t ethanolPct; //Flex sensor value (or 0 if not used)
+	uint8_t flexCorrection; //Flex fuel correction (% above or below 100)
+	uint8_t flexIgnCorrection; //Ignition correction (Increased degrees of advance) for flex fuel
+	uint8_t idleLoad;
+	uint8_t testOutputs; // testEnabled(0), testActive(1)
+	uint8_t O2_2; //O2
+	uint8_t baro; //Barometer value
+	uint16_t CANin_1;
+	uint16_t CANin_2;
+	uint16_t CANin_3;
+	uint16_t CANin_4;
+	uint16_t CANin_5;
+	uint16_t CANin_6;
+	uint16_t CANin_7;
+	uint16_t CANin_8;
+	uint16_t CANin_9;
+	uint16_t CANin_10;
+	uint16_t CANin_11;
+	uint16_t CANin_12;
+	uint16_t CANin_13;
+	uint16_t CANin_14;
+	uint16_t CANin_15;
+	uint16_t CANin_16;
+	uint8_t tpsADC;
+	uint8_t getNextError;
+	uint8_t launchCorrection;
+};
+
+statuses currentStatus;
 
 static uint32_t oldtime=millis();   // for the timeout
 byte SpeedyResponse[100]; //The data buffer for the serial3 data. This is longer than needed, just in case
@@ -42,6 +110,7 @@ int CLT;   // to store coolant temp
 unsigned int RPM,PW,PWcount;   //RPM and PW from speeduino
 byte TPS,tempLight;   //TPS value and overheat light on/off
 bool data_error; //indicator for the data from speeduino being ok.
+uint8_t data[255]; //For DS2 data
 
 #if ((STM32_CORE_VERSION_MINOR<=8) & (STM32_CORE_VERSION_MAJOR==1))
 void SendData(HardwareTimer*){void SendData();}
@@ -51,6 +120,10 @@ void setup(){
   pinMode(pin, OUTPUT);
   Serial3.begin(115200);  // baudrate for Speeduino is 115200
   Serial.begin(115200); // for debugging
+  #ifdef DS2_ENABLE
+  Serial2.begin(9600, SERIAL_8E1);
+  Serial2.setTimeout(ISO_TIMEOUT);
+  #endif
   
   Can1.begin();
   Can1.setBaudRate(500000);
@@ -91,7 +164,7 @@ void setup(){
 // zero the data to be sent by CAN bus, so it's not just random garbage
 
   CLT = 0;
-  PW = 0;
+  currentStatus.PW1 = 0;
   PWcount = 0;
   rpmLSB = 0;
   rpmMSB = 0;
@@ -119,6 +192,79 @@ void setup(){
   
   requestData(); // all set. Start requesting data from speeduino
 }
+
+#ifdef DS2_ENABLE
+uint32_t convertValue(float val, float mul = 1, float add = 0) {
+  // uint32_t meaning we can set up to 4 bytes this way
+  uint32_t convertedVal = (uint32_t) ((val - add)/mul); // we need to reverse what we do on logger side
+  return convertedVal;
+}
+
+void addValToData(uint32_t val, uint8_t data[], uint8_t offset, uint8_t length = 1, bool reverseEndianess = false) {
+  for(uint8_t i = 0; i < length; i++) {
+    if(reverseEndianess) data[offset + i] = ((uint8_t *)&val)[i];
+    else data[offset + i] = ((uint8_t *)&val)[3-i];
+  }
+}
+
+void sendReply(uint8_t data[]) {
+  data[0] = 0x12; // Not needed as our data already have that
+  data[1] = 32; // because our highest known offset is 32, if we know all parameters we need to double check if it's acutally ok value
+
+  // I assume we don't need to change it, would need to see MS42 raw reply to confirm
+  data[2] = 0x0B;
+  data[3] = 0x03;
+  uint8_t offset = 3;
+
+  // Here is where payload starts:
+
+  float EngineSpeedVal = currentStatus.RPM;
+  uint8_t EngineSpeedOffset = 0;
+  uint8_t EngineSpeedLength = 2;
+  uint32_t valueToSend = convertValue(EngineSpeedVal, 0.0015258789, -50);
+  addValToData(valueToSend, data, EngineSpeedOffset + offset, EngineSpeedLength);
+
+
+  float lambdaInt1Val = currentStatus.egoCorrection;
+  uint8_t lambdaInt1Offset = 21;
+  uint8_t lambdaInt1Length = 2;
+  valueToSend = convertValue(lambdaInt1Val, 0.0015258789, -50);
+  addValToData(valueToSend, data, lambdaInt1Offset + offset, lambdaInt1Length);
+
+// DO MORE STUFF HERE
+  
+  // Checksum
+  uint8_t checksum = 0;
+  for(uint8_t i = 0; i < data[1]-1; i++) {
+    checksum ^= data[i]; // Simple XOR checksum
+  }
+  data[data[1]-1] = checksum;
+  DS2.sendCommand(data);
+  DS2.clearRX(); // we clear RX on our side so we don't read the stuff that we sent out
+}
+
+void sendEcuId(uint8_t data[]) {
+  data[0] = 0x12; // Not really needed as our data already have this
+  data[1] = 10;   // response length
+  data[2] = 0xA0; //Ack
+  data[3] = 37;  //Ecu Id in ASCII
+  data[4] = 35;
+  data[5] = 30;
+  data[6] = 30;
+  data[7] = 32;
+  data[8] = 35;
+  data[9] = 35;
+  
+    // Checksum
+  uint8_t checksum = 0;
+  for(uint8_t i = 0; i < data[1]-1; i++) {
+    checksum ^= data[i]; // Simple XOR checksum
+  }
+  data[data[1]-1] = checksum;
+  DS2.sendCommand(data);
+  DS2.clearRX(); // we clear RX on our side so we don't read the stuff that we sent out
+}
+#endif
 
 void requestData() {
   Serial3.write("A"); //Send A to request real time data
@@ -172,7 +318,7 @@ void SendData()   // Send can messages in 32Hz phase from timer interrupt. This 
   else{
     CAN_msg_MPG_CEL.buf[0]= 0x00;  //CEL off
   }
-  updatePW = updatePW + ( PW * (RPM/1000) );
+  updatePW = updatePW + ( currentStatus.PW1 * (RPM/1000) );
   PWcount = (updatePW/5500); // fuel consumption is measured by rate of change in instrument cluster. So PW counter is increased by steps of one. And rate of update depends on PW from speeduino. This isn't yet working correctly.
     if (PWcount == 0xFFFF)
     {
@@ -197,8 +343,6 @@ void displayData(){
 }
 
 void processData(){   // necessary conversion for the data before sending to CAN BUS
-  byte tempTPS;
-  byte tempCLT;
   unsigned int tempRPM;
   data_error = false; // set the received data as ok
 
@@ -207,16 +351,31 @@ void processData(){   // necessary conversion for the data before sending to CAN
       data_error = true; //data received is probaply corrupted, don't use it.
       Serial.print ("Not an A message");
     }
-
-  tempCLT       = SpeedyResponse[8];
-  RPM           = ((SpeedyResponse [16] << 8) | (SpeedyResponse [15])); // RPM low & high (Int) TBD: probaply no need to split high and low bytes etc. this could be all simpler
-  PW            = ((SpeedyResponse [22] << 8) | (SpeedyResponse [21])); // PW low & high (Int) TBD: probaply no need to split high and low bytes etc. this could be all simpler
-  tempTPS       = SpeedyResponse[25];
+  currentStatus.secl = SpeedyResponse[1];
+  currentStatus.status1 = SpeedyResponse[2];
+  currentStatus.engine = SpeedyResponse[3];
+  currentStatus.dwell = SpeedyResponse[4];
+  currentStatus.MAP = ((SpeedyResponse [6] << 8) | (SpeedyResponse [5]));
+  currentStatus.IAT = SpeedyResponse[7];
+  currentStatus.CLT = SpeedyResponse[8];
+  currentStatus.batCorrection = SpeedyResponse[9];
+  currentStatus.battery10 = SpeedyResponse[10];
+  currentStatus.O2 = SpeedyResponse[11];
+  currentStatus.egoCorrection = SpeedyResponse[12];
+  currentStatus.iatCorrection = SpeedyResponse[13];
+  currentStatus.wueCorrection = SpeedyResponse[14];
+  currentStatus.RPM = ((SpeedyResponse [16] << 8) | (SpeedyResponse [15])); // RPM low & high (Int) TBD: probaply no need to split high and low bytes etc. this could be all simpler
+  currentStatus.AEamount = SpeedyResponse[17];
+  currentStatus.corrections = SpeedyResponse[18];
+  currentStatus.VE = SpeedyResponse[19];
+  currentStatus.afrTarget = SpeedyResponse[20];
+  currentStatus.PW1 = ((SpeedyResponse [22] << 8) | (SpeedyResponse [21])); // PW low & high (Int) TBD: probaply no need to split high and low bytes etc. this could be all simpler
+  currentStatus.TPS = SpeedyResponse[25];
 
   // check if received values makes sense and convert those if all is ok.
-  if (RPM < 8000 && data_error == false)  // the engine will not probaply rev over 8000 RPM
+  if (currentStatus.RPM < 8000 && data_error == false)  // the engine will not probaply rev over 8000 RPM
   {
-    tempRPM = RPM * 6.4; // RPM conversion factor for e46/e39 cluster
+    tempRPM = currentStatus.RPM * 6.4; // RPM conversion factor for e46/e39 cluster
     rpmMSB = tempRPM >> 8;  //split to high and low byte
     rpmLSB = tempRPM;
   }
@@ -226,10 +385,10 @@ void processData(){   // necessary conversion for the data before sending to CAN
     Serial.print ("Error. RPM Received:"); Serial.print (RPM); Serial.print("\t");
   }
 
-  if (tempCLT < 182 && data_error == false)  // 142 degrees Celcius is the hottest temp that fits to the conversion. 
+  if (currentStatus.CLT < 182 && data_error == false)  // 142 degrees Celcius is the hottest temp that fits to the conversion. 
   {
-    CLT = (tempCLT -40)*4/3+64;  // CLT conversion factor for e46/e39 cluster
-    if (tempCLT > 160) {
+    CLT = (currentStatus.CLT -40)*4/3+64;  // CLT conversion factor for e46/e39 cluster
+    if (currentStatus.CLT > 160) {
       tempLight = 0x8;  // turn on engine temp warning light after 120 degrees Celcius
     }
     else
@@ -240,17 +399,17 @@ void processData(){   // necessary conversion for the data before sending to CAN
   else
   {
     data_error = true;//data received is probaply corrupted, don't use it.
-    Serial.print ("Error. CLT received:"); Serial.print (tempCLT); Serial.print("\t");
+    Serial.print ("Error. CLT received:"); Serial.print (currentStatus.CLT); Serial.print("\t");
   }
 
-  if (tempTPS < 101 && data_error == false)  //TPS values can only be from 0-100
+  if (currentStatus.TPS < 101 && data_error == false)  //TPS values can only be from 0-100
   {
-    TPS = map(tempTPS, 0, 100, 1, 254); //0-100 TPS value mapped to 0x01 to 0xFE range.
+    TPS = map(currentStatus.TPS, 0, 100, 1, 254); //0-100 TPS value mapped to 0x01 to 0xFE range.
   }
   else
   {
     data_error = true; //data received is probaply corrupted, don't use it.
-    Serial.print ("Error. TPS received:"); Serial.print (tempTPS); Serial.print("\t");
+    Serial.print ("Error. TPS received:"); Serial.print (currentStatus.TPS); Serial.print("\t");
   }
 }
 
@@ -283,5 +442,18 @@ void loop() {
   while (Can1.read(CAN_inMsg) ) 
   {
     readCanMessage();
+  }
+  // Read DS2 command and respond to it
+  if(DS2.readCommand(data)) {
+    uint8_t len = data[1];
+    switch(data[2]) {
+      case 0x0B:
+        if(data[3] == 0x03) sendReply(data);
+        break;
+      case 0x00:
+        if(data[3] == 0x03) sendEcuId(data);
+        break;
+      default: break;
+    }
   }
 }
