b84ceac25c130052b7a636158826f25fcf07f7bb
Pasi Kemppainen <48950874+pazi88@users.noreply.github.com>
2021-11-10 13:52:04 +0200

    Update STM32_CAN.h

--- m52tu, m54 PnP/Serial3toBMWcan/src/STM32_CAN/STM32_CAN.h
+++ m52tu, m54 PnP/Serial3toBMWcan/src/STM32_CAN/STM32_CAN.h
@@ -1,12 +1,15 @@
 /*
-This is CAN library for STM32 to be used in Speeduino engine management system by pazi88.
-The library is created because at least currently (year 2021) there is no CAN library in the STM32 core.
+This is universal CAN library for STM32 that was made to be used with Speeduino EFI and in my other projects.
+It should support all STM32 MCUs that are also supported in stm32duino Arduino_Core_STM32 and supports up to 3x CAN busses.
+The library is created because at least currently (year 2021) there is no official CAN library in the STM32 core.
 This library is based on several STM32 CAN example libraries linked below and it has been combined with few
 things from Teensy FlexCAN library to make it compatible with the CAN features that exist in speeduino for Teensy.
 Links to repositories that have helped with this:
 https://github.com/nopnop2002/Arduino-STM32-CAN
 https://github.com/J-f-Jensen/libraries/tree/master/STM32_CAN
 https://github.com/jiauka/STM32F1_CAN
+
+STM32 core: https://github.com/stm32duino/Arduino_Core_STM32
 */
 
 #ifndef STM32_CAN_H
